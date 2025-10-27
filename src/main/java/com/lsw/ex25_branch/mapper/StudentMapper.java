3050df8caa31b8031cfc9ca782d7fb9d83a3d079
lsw1232414 <limsw980821@gmail.com>
2025-10-27 14:25:54 +0900

    a

--- src/main/java/com/lsw/ex25_branch/mapper/StudentMapper.java
+++ src/main/java/com/lsw/ex25_branch/mapper/StudentMapper.java
@@ -12,22 +12,10 @@ public interface StudentMapper {
 	List<Student> findAll();
 
 	Student findById(Long id);
-//	@Select("SELECT * FROM student WHERE id = #{id}")
-//	Student findById(Long id);
 
 	void insert(Student student);
-//	@Insert("INSERT INTO student(NAME, email, age) \r\n"
-//			+ "  	VALUES (#{name}, #{email}, #{age})")
-//	@Options(useGeneratedKeys = true, keyProperty = "id")
-//	void insert(Student student);
 
 	void update(Student student);
-//	@Update("UPDATE student\r\n"
-//			+ "	SET NAME = #{name}, email = #{email}, age = #{age}\r\n"
-//			+ "	WHERE id = #{id}")
-//	void update(Student student);
 
 	void delete(Long id);
-//	@Delete("DELETE FROM student WHERE id = #{id}")
-//	void delete(Long id);
 }
